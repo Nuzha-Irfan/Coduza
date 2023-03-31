a192021cb62a44c67e5797868ca0a68e69bef6bc
Nuzha-Irfan <nuzhaxyz@outlook.com>
2023-03-31 22:12:58 +0530

    docker

--- server.js
+++ server.js
@@ -16,15 +16,15 @@ app.use(express.json({ extended: false }));
 app.use('/api/joke', require('./routes/api/jokes'));
 app.use('/api/auth', require('./routes/api/auth'));
 
-// // server static assets in production
-// if (process.env.NODE_ENV === 'production') {
-//   // set static folder
-//   app.use(express.static('client/build'));
+// server static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('frontend/build'));
 
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
