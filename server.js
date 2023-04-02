69170f0e2de98b29a19c8fe12af7f71dc1e48428
Nuzha-Irfan <nuzhaxyz@outlook.com>
2023-04-02 15:01:28 +0530

    added build files

--- server.js
+++ server.js
@@ -16,15 +16,7 @@ app.use(express.json({ extended: false }));
 app.use('/api/joke', require('./routes/api/jokes'));
 app.use('/api/auth', require('./routes/api/auth'));
 
-// server static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // set static folder
-  app.use(express.static('frontend/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  });
-}
 
 const PORT = process.env.PORT || 5000;
 
